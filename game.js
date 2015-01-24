8e57edd55f6b928b724aab3f076890362bd7aed5
Jonathan Frazier <jfrazier@redeapp.com>
2015-01-24 17:24:41 -0500

    Basic infinite scroll with obstacle generation.

--- game.js
+++ game.js
@@ -18,9 +18,14 @@ var manifest = {
 
 var game = new Splat.Game(canvas, manifest);
 
-function generatePositions(canvas, playerSize){
-    var centerLane = canvas.width/2 - playerSize/2;
+function generatePositions(canvas, player){
     var laneWidth = 100;
+
+    var centerLane = canvas.width/2 - player.width/2;
+    var renderDistance = canvas.height*(7/8);
+
+    player.x = centerLane;
+    player.y = renderDistance;
 
     return {
         lanes: [
@@ -31,13 +36,15 @@ function generatePositions(canvas, playerSize){
 
         leftBound: canvas.width/2 - canvas.width*0.2 + 100,
         rightBound: canvas.width/2 + canvas.width*0.2 - 100,
-        playerStart: canvas.height*(7/8),
-        obstacleStart: canvas.height*(1/8)
+        renderDistance: renderDistance,
+        obstacleStart: function() { return player.y - renderDistance; }
     };
 }
 
-// function spawnObstacle(){
-// }
+function spawnObstacle(positions){
+    return new Splat.Entity(positions.lanes[randomNumber(3)],
+                            positions.obstacleStart(), 40, 40);
+}
 
 function randomNumber(max) {
     return Math.floor((Math.random() * max));
@@ -58,7 +65,6 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
     // initialization
 }, function() {
     // simulation
-
     if(game.keyboard.consumePressed("space")){
 	game.scenes.switchTo("main");
     }
@@ -73,22 +79,26 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 }));
 
 game.scenes.add("main", new Splat.Scene(canvas, function() {
-    var playerSize = 50;
-    var Positions = generatePositions(canvas, playerSize);
-
     // initialization
-    this.player = new Splat.Entity(Positions.lanes[1],Positions.playerStart,playerSize,playerSize);
-    this.player.color = "red";
-    this.player.vy = -1;
+    var playerSize = 50;
+    // var playerImage = game.images.get("runman-idle");
 
-    this.obstacle = new Splat.Entity(Positions.lanes[randomNumber(3)],
-                                     Positions.obstacleStart, 40, 40);
+    this.player = new Splat.Entity(canvas.width/2 - 25,canvas.height*(7/8),playerSize,playerSize); 
 
     this.camera = new Splat.EntityBoxCamera(this.player,
                                             canvas.width, canvas.height*(1/8),
                                             canvas.width/2, canvas.height*(15/16));
 
-    this.positions = Positions;
+
+    this.positions = generatePositions(canvas, this.player);
+
+    this.player.color = "red";
+    this.player.vy = -1;
+
+    this.obstacles = [ spawnObstacle(this.positions) ];
+
+    // this.player = new Splat.AnimatedEntity(Positions.lanes[1],Positions.playerStart,
+    //                                        playerImage.width,playerImage.height,playerImage,0,0);
 }, function(elapsedMs) {
     this.player.move(elapsedMs);
     
@@ -102,15 +112,22 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
        this.player.x < this.positions.rightBound){
 	this.player.x += 150;
     }
+
+    if(game.keyboard.consumePressed("o")) {
+        this.obstacles.push(spawnObstacle(this.positions));
+    }
+
 }, function(context) {
     // draw
     context.fillStyle = "#ffffff";
-    context.fillRect(canvas.width/2 - canvas.width*0.2, this.player.y - this.positions.playerStart,
+    context.fillRect(canvas.width/2 - canvas.width*0.2, this.player.y - this.positions.renderDistance,
                      canvas.width*0.4, canvas.height);
 
     drawEntity(context, this.player);
 
-    drawEntity(context, this.obstacle);
+    for(var i = 0; i < this.obstacles.length; i++) {
+        drawEntity(context, this.obstacles[i]);
+    }
 }));
 
 game.scenes.add("plane", new Splat.Scene(canvas, function() {
@@ -143,7 +160,6 @@ game.scenes.add("plane", new Splat.Scene(canvas, function() {
     context.fillStyle = "#ffffff";
     context.fillRect(0, canvas.height/2 - canvas.height*0.2, canvas.width, canvas.height*0.4);
     drawEntity(context,this.player);
-    // this.player.draw(context);
 }));
 
 game.scenes.switchTo("loading");
